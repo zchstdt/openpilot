f26ea1651cb6d6c5f04af0019a72a799de542e18
Harald Schafer <harald.the.engineer@gmail.com>
2021-01-20 20:38:22 -0800

    relax also

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -215,7 +215,7 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.03]]
-      ret.lateralTuning.pid.kf = 0.00007818594
+      ret.lateralTuning.pid.kf = 0.000007818594
 
     elif candidate in [CAR.LEXUS_ES_TSS2, CAR.LEXUS_ESH_TSS2]:
       stop_and_go = True
