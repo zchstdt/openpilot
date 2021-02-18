53564b092e458f948e7f4cd8237ed64483172fa5
Robbe Derks <robbe.derks@gmail.com>
2021-02-18 09:23:20 +0100

    fix carstate bug

--- selfdrive/car/tesla/carstate.py
+++ selfdrive/car/tesla/carstate.py
@@ -26,8 +26,8 @@ class CarState(CarStateBase):
     ret.brakePressed = bool(cp.vl["DI_torque2"]["DI_brakePedal"])
 
     # Steering wheel
-    ret.steeringAngle = cp.vl["STW_ANGLHP_STAT"]["StW_AnglHP"]
-    ret.steeringRate = cp.vl["STW_ANGLHP_STAT"]["StW_AnglHP_Spd"]
+    ret.steeringAngleDeg = cp.vl["STW_ANGLHP_STAT"]["StW_AnglHP"]
+    ret.steeringRateDeg = cp.vl["STW_ANGLHP_STAT"]["StW_AnglHP_Spd"]
     ret.steeringTorque = cp.vl["EPAS_sysStatus"]["EPAS_torsionBarTorque"]
     ret.steeringPressed = (cp.vl["EPAS_sysStatus"]["EPAS_handsOnLevel"] > 0)
     ret.steerError = bool(cp.vl["EPAS_sysStatus"]["EPAS_steeringFault"])
